93327b4153a4ca7cfd449283f50eb40ae49361dc
mohammedharis041 <mohammedharis041@gmail.com>
2026-05-24 03:35:55 +0400

    Bump versionCode to 9 and versionName to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mhs.player"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Optimize resources: only keep relevant languages
