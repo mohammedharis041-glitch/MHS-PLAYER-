ab5a6a5ba650686a8f0c458bd1c8257cfd601474
mohammedharis041 <mohammedharis041@gmail.com>
2026-05-24 04:00:32 +0400

    Remove splits configuration to build only a single unified universal APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,14 +25,6 @@ android {
 
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
-            isUniversalApk = true
-        }
-    }
 
     buildTypes {
         release {
