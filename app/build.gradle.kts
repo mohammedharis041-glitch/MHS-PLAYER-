3212161cbfe966d0acc9efb0c094a36bf8932234
mohammedharis041 <mohammedharis041@gmail.com>
2026-05-30 03:17:38 +0400

    release: bump version to v1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mhs.player"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.1.4"
+        versionCode = 16
+        versionName = "1.1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Optimize resources: only keep relevant languages
